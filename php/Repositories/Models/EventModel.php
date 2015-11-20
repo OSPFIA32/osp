afb8d82956f088c6cbd705fbf3009d4d2d3ace20
Markus Laubenthal <markus.laubenthal@german-transfer.com>
2015-11-20 17:46:51 +0100

    verlasse jetzt die arbeit

--- php/Repositories/Models/EventModel.php
+++ php/Repositories/Models/EventModel.php
@@ -3,21 +3,34 @@
 class EventModel {
 
   /**
+  * @property $id
   * stores users id (LDAP)
-  *
   */
   public $id;
 
   /**
+  * @property $eventName
   * stores the users full name
-  *
   */
   public $eventName;
 
+  /**
+  * @property $startDate
+  * stores the events start date
+  */
+  public $startDate;
+
+  /**
+  * @property $endDate
+  * stores the events end date
+  */
+  public $endDate;
 
   public function __construct($id) {
     $this->id = (int)$id;
-    $this->eventName = 'Das ist ein Event';
+    $this->eventName = 'Deutschunterricht';
+    $this->startDate = '26.10.2015 07:45:00';
+    $this->endDate = '26.10.2015 09:15:00';
   }
 
   public function toArray() {
