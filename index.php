d439ce9cae82747bb9ba14f22ccbb6b65a062ba7
Relpat <patrickhippler@googlemail.com>
2015-11-22 19:12:50 +0100

    Meow

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+// Credentials
+
+
+// Connection
+require_once 'php/Classes/Connection.php';
+
+// Router
+require_once 'php/Classes/Router.php';
+
+// Controller
+require_once 'php/Controller/Login.php';
+require_once 'php/Controller/Events.php';
+require_once 'php/Controller/Default.php';
+require_once 'php/Controller/Impressum.php';
+
+// REST API
+require_once 'php/Controller/REST/User.php';
+require_once 'php/Controller/REST/Event.php';
+
+// LDAP API
+
+// Repositories
+require_once 'php/Repositories/UserRepository.php';
+require_once 'php/Repositories/EventRepository.php';
+
+// View
+require_once 'php/Classes/View.php';
+
+// Merge $_GET and $_POST
+$request = array_merge($_GET, $_POST);
+// Create Controller and print content
+echo Router::getController($request)->display();
+
+
+
+
+?>
